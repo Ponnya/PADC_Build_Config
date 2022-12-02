d1dfb68746df2639b0c4cd86b9480f57c0fccf93
Ponnya <mr.ponnya@gmail.com>
2022-12-03 01:22:28 +0630

    -used share method from rider and consumer app

--- consumer/src/main/java/com/padc/ponnya/padcbuildconfig/MainActivity.kt
+++ consumer/src/main/java/com/padc/ponnya/padcbuildconfig/MainActivity.kt
@@ -2,10 +2,13 @@ package com.padc.ponnya.padcbuildconfig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.padc.ponnya.shared.showSnackBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showSnackBar("This is consumer app")
     }
 }
